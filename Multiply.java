29e8930553e07e2fcaee56e4290e18eb54f516d1
Rana De <ranacomp@gmail.com>
2023-07-21 13:02:39 +0530

    Added multiply java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, mul;  
+mul = n1 * n2;  
+System.out.println("Mul of 2 number is : "+sum);  
+}  
+}  
